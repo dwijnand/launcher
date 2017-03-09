d33535cd3507c213c404892c044056fc7647bbcf
Dale Wijnand <dale.wijnand@gmail.com>
2017-03-09 14:27:23 +0000

    Make 3 files easier to study & maintain

--- launcher-implementation/src/main/scala/xsbt/boot/Configuration.scala
+++ launcher-implementation/src/main/scala/xsbt/boot/Configuration.scala
@@ -4,39 +4,39 @@
 package xsbt.boot
 
 import Pre._
-import java.io.{ File, FileInputStream, InputStreamReader }
+import java.io.{ File, InputStreamReader }
 import java.net.{ MalformedURLException, URI, URL }
 import java.util.regex.Pattern
+import scala.annotation.tailrec
 import scala.collection.immutable.List
-import annotation.tailrec
-
-object ConfigurationStorageState extends Enumeration {
-  val PropertiesFile = value("properties-file")
-  val SerializedFile = value("serialized-file")
-}
 
 object Configuration {
   import ConfigurationStorageState._
-  final val SysPropPrefix = "-D"
-  def parse(file: URL, baseDirectory: File) = Using(new InputStreamReader(file.openStream, "utf8"))((new ConfigurationParser).apply)
 
-  /**
-   * Finds the configuration location.
-   *
-   * Note:  Configuration may be previously serialized by a launcher.
-   */
-  @tailrec def find(args: List[String], baseDirectory: File): (URL, List[String], ConfigurationStorageState.Value) =
+  final val SysPropPrefix = "-D"
+
+  def parse(file: URL, baseDirectory: File): LaunchConfiguration =
+    Using(new InputStreamReader(file.openStream, "utf8"))((new ConfigurationParser).apply)
+
+  /** Finds the configuration location. Note: Configuration may be previously serialized by a launcher. */
+  @tailrec
+  def find(args: List[String], baseDirectory: File): (URL, List[String], ConfigurationStorageState.Value) =
     args match {
-      case head :: tail if head.startsWith("@load:") => (directConfiguration(head.substring(6), baseDirectory), tail, SerializedFile)
-      case head :: tail if head.startsWith("@")      => (directConfiguration(head.substring(1), baseDirectory), tail, PropertiesFile)
+      case head :: tail if head.startsWith("@load:") =>
+        (directConfiguration(head.substring(6), baseDirectory), tail, SerializedFile)
+      case head :: tail if head.startsWith("@") =>
+        (directConfiguration(head.substring(1), baseDirectory), tail, PropertiesFile)
       case head :: tail if head.startsWith(SysPropPrefix) =>
         setProperty(head stripPrefix SysPropPrefix)
         find(tail, baseDirectory)
       case _ =>
         val propertyConfigured = System.getProperty("sbt.boot.properties")
-        val url = if (propertyConfigured == null) configurationOnClasspath else configurationFromFile(propertyConfigured, baseDirectory)
+        val url =
+          if (propertyConfigured == null) configurationOnClasspath
+          else configurationFromFile(propertyConfigured, baseDirectory)
         (url, args, PropertiesFile)
     }
+
   def setProperty(head: String) {
     val keyValue = head.split("=", 2)
     if (keyValue.length != 2)
@@ -44,65 +44,65 @@ object Configuration {
     else
       toUnit(System.setProperty(keyValue(0), keyValue(1)))
   }
-  def configurationOnClasspath: URL =
-    {
-      val paths = resourcePaths(guessSbtVersion)
-      paths.iterator.map(getClass.getResource).find(neNull) getOrElse
-        (multiPartError("Could not finder sbt launch configuration.  Searched classpath for:", paths))
-    }
-  def directConfiguration(path: String, baseDirectory: File): URL =
-    {
-      try { new URL(path) }
-      catch { case _: MalformedURLException => configurationFromFile(path, baseDirectory) }
-    }
-  def configurationFromFile(path: String, baseDirectory: File): URL =
-    {
-      val pathURI = filePathURI(path)
-      def resolve(against: URI): Option[URL] =
-        {
-          val resolved = against.resolve(pathURI) // variant that accepts String doesn't properly escape (#725)
-          val exists = try { (new File(resolved)).exists } catch { case _: IllegalArgumentException => false }
-          if (exists) Some(resolved.toURL) else None
-        }
-      val against = resolveAgainst(baseDirectory)
-      // use Iterators so that resolution occurs lazily, for performance
-      val resolving = against.iterator.flatMap(e => resolve(e).toList.iterator)
-      if (!resolving.hasNext) multiPartError("Could not find configuration file '" + path + "'.  Searched:", against)
-      resolving.next()
-    }
-  def multiPartError[T](firstLine: String, lines: List[T]) = Pre.error((firstLine :: lines).mkString("\n\t"))
 
-  def UnspecifiedVersionPart = "Unspecified"
-  def DefaultVersionPart = "Default"
-  def DefaultBuildProperties = "project/build.properties"
-  def SbtVersionProperty = "sbt.version"
-  val ConfigurationName = "sbt.boot.properties"
-  val JarBasePath = "/sbt/"
-  def userConfigurationPath = "/" + ConfigurationName
-  def defaultConfigurationPath = JarBasePath + ConfigurationName
+  def configurationOnClasspath: URL = {
+    val paths = resourcePaths(guessSbtVersion)
+    paths.iterator.map(getClass.getResource).find(neNull) getOrElse
+      (multiPartError("Could not finder sbt launch configuration.  Searched classpath for:", paths))
+  }
+
+  def directConfiguration(path: String, baseDirectory: File): URL =
+    try new URL(path)
+    catch { case _: MalformedURLException => configurationFromFile(path, baseDirectory) }
+
+  def configurationFromFile(path: String, baseDirectory: File): URL = {
+    val pathURI = filePathURI(path)
+    def resolve(against: URI): Option[URL] = {
+      val resolved = against.resolve(pathURI) // variant that accepts String doesn't properly escape (#725)
+      val exists = try new File(resolved).exists
+      catch { case _: IllegalArgumentException => false }
+      if (exists) Some(resolved.toURL) else None
+    }
+    val against = resolveAgainst(baseDirectory)
+    // use Iterators so that resolution occurs lazily, for performance
+    val resolving = against.iterator.flatMap(e => resolve(e).toList.iterator)
+    if (!resolving.hasNext)
+      multiPartError(s"Could not find configuration file '$path'.  Searched:", against)
+    resolving.next()
+  }
+
+  def multiPartError[T](firstLine: String, lines: List[T]): Nothing =
+    Pre.error((firstLine :: lines).mkString("\n\t"))
+
+  def UnspecifiedVersionPart: String = "Unspecified"
+  def DefaultVersionPart: String = "Default"
+  def DefaultBuildProperties: String = "project/build.properties"
+  def SbtVersionProperty: String = "sbt.version"
+  val ConfigurationName: String = "sbt.boot.properties"
+  val JarBasePath: String = "/sbt/"
+  def userConfigurationPath: String = s"/$ConfigurationName"
+  def defaultConfigurationPath: String = JarBasePath + ConfigurationName
   val baseResourcePaths: List[String] = userConfigurationPath :: defaultConfigurationPath :: Nil
+
   def resourcePaths(sbtVersion: Option[String]): List[String] =
-    versionParts(sbtVersion) flatMap { part =>
-      baseResourcePaths map { base =>
-        base + part
-      }
-    }
+    for (part <- versionParts(sbtVersion); base <- baseResourcePaths) yield base + part
+
   def fallbackParts: List[String] = "" :: Nil
-  def versionParts(version: Option[String]): List[String] =
-    version match {
-      case None    => UnspecifiedVersionPart :: fallbackParts
-      case Some(v) => versionParts(v)
-    }
-  def versionParts(version: String): List[String] =
-    {
-      val pattern = Pattern.compile("""(\d+)(\.\d+)(\.\d+)(-.*)?""")
-      val m = pattern.matcher(version)
-      if (m.matches())
-        subPartsIndices flatMap { is => fullMatchOnly(is.map(m.group)) }
-      else
-        noMatchParts
-    }
+
+  def versionParts(version: Option[String]): List[String] = version match {
+    case None    => UnspecifiedVersionPart :: fallbackParts
+    case Some(v) => versionParts(v)
+  }
+
+  def versionParts(version: String): List[String] = {
+    val pattern = Pattern.compile("""(\d+)(\.\d+)(\.\d+)(-.*)?""")
+    val m = pattern.matcher(version)
+    if (m.matches()) subPartsIndices flatMap (is => fullMatchOnly(is.map(m.group)))
+    else noMatchParts
+  }
+
   def noMatchParts: List[String] = DefaultVersionPart :: fallbackParts
+
   private[this] def fullMatchOnly(groups: List[String]): Option[String] =
     if (groups.forall(neNull)) Some(groups.mkString) else None
 
@@ -113,14 +113,13 @@ object Configuration {
       (Nil) ::
       Nil
 
-  // the location of project/build.properties and the name of the property within that file 
+  // the location of project/build.properties and the name of the property within that file
   //  that configures the sbt version is configured in sbt.boot.properties.
   // We have to hard code them here in order to use them to determine the location of sbt.boot.properties itself
-  def guessSbtVersion: Option[String] =
-    {
-      val props = Pre.readProperties(new File(DefaultBuildProperties))
-      Option(props.getProperty(SbtVersionProperty))
-    }
+  def guessSbtVersion: Option[String] = {
+    val props = Pre.readProperties(new File(DefaultBuildProperties))
+    Option(props.getProperty(SbtVersionProperty))
+  }
 
   def resolveAgainst(baseDirectory: File): List[URI] =
     directoryURI(baseDirectory) ::
@@ -128,28 +127,25 @@ object Configuration {
       toDirectory(classLocation(getClass).toURI) ::
       Nil
 
-  def classLocation(cl: Class[_]): URL =
-    {
-      val codeSource = cl.getProtectionDomain.getCodeSource
-      if (codeSource == null) Pre.error("No class location for " + cl)
-      else codeSource.getLocation
-    }
-  // single-arg constructor doesn't properly escape
-  def filePathURI(path: String): URI = {
-    if (path.startsWith("file:")) new URI(path)
-    else {
-      val f = new File(path)
-      new URI(if (f.isAbsolute) "file" else null, path, null)
-    }
+  def classLocation(cl: Class[_]): URL = {
+    val codeSource = cl.getProtectionDomain.getCodeSource
+    if (codeSource == null) Pre.error("No class location for " + cl)
+    else codeSource.getLocation
   }
+
+  // single-arg constructor doesn't properly escape
+  def filePathURI(path: String): URI =
+    if (path.startsWith("file:")) new URI(path)
+    else new URI(if (new File(path).isAbsolute) "file" else null, path, null)
+
   def directoryURI(dir: File): URI = directoryURI(dir.toURI)
-  def directoryURI(uri: URI): URI =
-    {
-      assert(uri.isAbsolute)
-      val str = uri.toASCIIString
-      val dirStr = if (str.endsWith("/")) str else str + "/"
-      (new URI(dirStr)).normalize
-    }
+
+  def directoryURI(uri: URI): URI = {
+    assert(uri.isAbsolute)
+    val str = uri.toASCIIString
+    val dirStr = if (str.endsWith("/")) str else s"$str/"
+    new URI(dirStr).normalize
+  }
 
   def toDirectory(uri: URI): URI =
     try {
@@ -157,5 +153,11 @@ object Configuration {
       val newFile = if (file.isFile) file.getParentFile else file
       directoryURI(newFile)
     } catch { case _: Exception => uri }
+
   private[this] def neNull: AnyRef => Boolean = _ ne null
+}
+
+object ConfigurationStorageState extends Enumeration {
+  val PropertiesFile = value("properties-file")
+  val SerializedFile = value("serialized-file")
 }
--- launcher-implementation/src/main/scala/xsbt/boot/LaunchConfiguration.scala
+++ launcher-implementation/src/main/scala/xsbt/boot/LaunchConfiguration.scala
@@ -9,134 +9,262 @@ import java.net.URL
 import scala.collection.immutable.List
 
 //TODO: use copy constructor, check size change
-final case class LaunchConfiguration(scalaVersion: Value[String], ivyConfiguration: IvyOptions, app: Application, boot: BootSetup, logging: Logging, appProperties: List[AppProperty], serverConfig: Option[ServerConfiguration]) {
+final case class LaunchConfiguration(
+  scalaVersion: Value[String],
+  ivyConfiguration: IvyOptions,
+  app: Application,
+  boot: BootSetup,
+  logging: Logging,
+  appProperties: List[AppProperty],
+  serverConfig: Option[ServerConfiguration]
+) {
+
   def isServer: Boolean = serverConfig.isDefined
-  def getScalaVersion = {
+
+  def getScalaVersion: Option[String] = {
     val sv = Value.get(scalaVersion)
     if (sv == "auto") None else Some(sv)
   }
 
-  def withScalaVersion(newScalaVersion: String) = LaunchConfiguration(new Explicit(newScalaVersion), ivyConfiguration, app, boot, logging, appProperties, serverConfig)
-  def withApp(app: Application) = LaunchConfiguration(scalaVersion, ivyConfiguration, app, boot, logging, appProperties, serverConfig)
-  def withAppVersion(newAppVersion: String) = LaunchConfiguration(scalaVersion, ivyConfiguration, app.withVersion(new Explicit(newAppVersion)), boot, logging, appProperties, serverConfig)
-  // TODO: withExplicit
-  def withVersions(newScalaVersion: String, newAppVersion: String, classifiers0: Classifiers) =
-    LaunchConfiguration(new Explicit(newScalaVersion), ivyConfiguration.copy(classifiers = classifiers0), app.withVersion(new Explicit(newAppVersion)), boot, logging, appProperties, serverConfig)
+  def withScalaVersion(newScalaVersion: String): LaunchConfiguration = {
+    val newScalaVersion1 = new Explicit(newScalaVersion)
+    LaunchConfiguration(newScalaVersion1, ivyConfiguration, app, boot, logging, appProperties, serverConfig)
+  }
 
-  def map(f: File => File) = LaunchConfiguration(scalaVersion, ivyConfiguration.map(f), app.map(f), boot.map(f), logging, appProperties, serverConfig.map(_ map f))
+  def withApp(app: Application): LaunchConfiguration =
+    LaunchConfiguration(scalaVersion, ivyConfiguration, app, boot, logging, appProperties, serverConfig)
+
+  def withAppVersion(newAppVersion: String): LaunchConfiguration = {
+    val newApp = app.withVersion(new Explicit(newAppVersion))
+    LaunchConfiguration(scalaVersion, ivyConfiguration, newApp, boot, logging, appProperties, serverConfig)
+  }
+
+  // TODO: withExplicit
+  def withVersions(
+    newScalaVersion: String,
+    newAppVersion: String,
+    classifiers0: Classifiers
+  ): LaunchConfiguration = {
+    val newScalaVersion1 = new Explicit(newScalaVersion)
+    val newIvyConfig = ivyConfiguration.copy(classifiers = classifiers0)
+    val newApp = app.withVersion(new Explicit(newAppVersion))
+    LaunchConfiguration(newScalaVersion1, newIvyConfig, newApp, boot, logging, appProperties, serverConfig)
+  }
+
+  def map(f: File => File) =
+    LaunchConfiguration(
+      scalaVersion,
+      ivyConfiguration.map(f),
+      app.map(f),
+      boot.map(f),
+      logging,
+      appProperties,
+      serverConfig.map(_ map f)
+    )
+
 }
+
 object LaunchConfiguration {
   // Saves a launch configuration into a file. This is only safe if it is loaded by the *same* launcher version.
-  def save(config: LaunchConfiguration, f: File): Unit = {
-    val out = new java.io.ObjectOutputStream(new java.io.FileOutputStream(f))
-    try out.writeObject(config)
-    finally out.close()
-  }
-  // Restores a launch configuration from a file. This is only safe if it is loaded by the *same* launcher version.  
+  def save(config: LaunchConfiguration, f: File): Unit =
+    andClose(new java.io.ObjectOutputStream(new java.io.FileOutputStream(f)))(_ writeObject config)
+
+  // Restores a launch configuration from a file. This is only safe if it is loaded by the *same* launcher version.
   def restore(url: URL): LaunchConfiguration = {
-    val in = new java.io.ObjectInputStream(url.openConnection.getInputStream)
-    try in.readObject.asInstanceOf[LaunchConfiguration]
-    finally in.close()
+    val obj = andClose(new java.io.ObjectInputStream(url.openConnection.getInputStream))(_.readObject)
+    obj.asInstanceOf[LaunchConfiguration]
   }
+
+  private[this] def andClose[A <: java.io.Closeable, B](x: A)(f: A => B): B =
+    try f(x)
+    finally x.close()
 }
+
 final case class ServerConfiguration(lockFile: File, jvmArgs: Option[File], jvmPropsFile: Option[File]) {
-  def map(f: File => File) =
+  def map(f: File => File): ServerConfiguration =
     ServerConfiguration(f(lockFile), jvmArgs map f, jvmPropsFile map f)
 }
-final case class IvyOptions(ivyHome: Option[File], classifiers: Classifiers, repositories: List[Repository.Repository], checksums: List[String], isOverrideRepositories: Boolean) {
-  def map(f: File => File) = IvyOptions(ivyHome.map(f), classifiers, repositories, checksums, isOverrideRepositories)
+
+final case class IvyOptions(
+  ivyHome: Option[File],
+  classifiers: Classifiers,
+  repositories: List[Repository.Repository],
+  checksums: List[String],
+  isOverrideRepositories: Boolean
+) {
+  def map(f: File => File): IvyOptions =
+    IvyOptions(ivyHome.map(f), classifiers, repositories, checksums, isOverrideRepositories)
 }
+
 sealed trait Value[T] extends Serializable
+
 final class Explicit[T](val value: T) extends Value[T] {
   override def toString = value.toString
 }
+
 final class Implicit[T](val name: String, val default: Option[T]) extends Value[T] {
   require(isNonEmpty(name), "Name cannot be empty")
-  override def toString = name + (default match { case Some(d) => "[" + d + "]"; case None => "" })
+  override def toString = name + default.fold("")(d => s"[$d]")
 }
+
 object Value {
-  def get[T](v: Value[T]): T = v match { case e: Explicit[T] => e.value; case _ => throw new BootException("Unresolved version: " + v) }
+  def get[T](v: Value[T]): T = v match {
+    case e: Explicit[T] => e.value
+    case _              => throw new BootException(s"Unresolved version: $v")
+  }
+
   def readImplied[T](s: String, name: String, default: Option[String])(implicit read: String => T): Value[T] =
-    if (s == "read") new Implicit(name, default map read) else Pre.error("Expected 'read', got '" + s + "'")
+    if (s == "read") new Implicit(name, default map read) else Pre.error(s"Expected 'read', got '$s'")
 }
 
 final case class Classifiers(forScala: Value[List[String]], app: Value[List[String]])
+
 object Classifiers {
-  def apply(forScala: List[String], app: List[String]): Classifiers = Classifiers(new Explicit(forScala), new Explicit(app))
+  def apply(forScala: List[String], app: List[String]): Classifiers =
+    Classifiers(new Explicit(forScala), new Explicit(app))
 }
 
 object LaunchCrossVersion {
-  def apply(s: String): xsbti.CrossValue =
-    s match {
-      case x if CrossVersionUtil.isFull(s)     => xsbti.CrossValue.Full
-      case x if CrossVersionUtil.isBinary(s)   => xsbti.CrossValue.Binary
-      case x if CrossVersionUtil.isDisabled(s) => xsbti.CrossValue.Disabled
-      case x                                   => Pre.error("Unknown value '" + x + "' for property 'cross-versioned'")
-    }
+  def apply(s: String): xsbti.CrossValue = s match {
+    case _ if CrossVersionUtil.isFull(s)     => xsbti.CrossValue.Full
+    case _ if CrossVersionUtil.isBinary(s)   => xsbti.CrossValue.Binary
+    case _ if CrossVersionUtil.isDisabled(s) => xsbti.CrossValue.Disabled
+    case _                                   => Pre.error(s"Unknown value '$s' for property 'cross-versioned'")
+  }
 }
 
-final case class Application(groupID: String, name: String, version: Value[String], main: String, components: List[String], crossVersioned: xsbti.CrossValue, classpathExtra: Array[File]) {
-  def getVersion = Value.get(version)
-  def withVersion(newVersion: Value[String]) = Application(groupID, name, newVersion, main, components, crossVersioned, classpathExtra)
-  def toID = AppID(groupID, name, getVersion, main, toArray(components), crossVersioned, classpathExtra)
-  def map(f: File => File) = Application(groupID, name, version, main, components, crossVersioned, classpathExtra.map(f))
+final case class Application(
+  groupID: String,
+  name: String,
+  version: Value[String],
+  main: String,
+  components: List[String],
+  crossVersioned: xsbti.CrossValue,
+  classpathExtra: Array[File]
+) {
+  def getVersion: String = Value.get(version)
+
+  def withVersion(newVersion: Value[String]): Application =
+    Application(groupID, name, newVersion, main, components, crossVersioned, classpathExtra)
+
+  def toID: AppID = AppID(groupID, name, getVersion, main, toArray(components), crossVersioned, classpathExtra)
+
+  def map(f: File => File): Application =
+    Application(groupID, name, version, main, components, crossVersioned, classpathExtra.map(f))
 }
-final case class AppID(groupID: String, name: String, version: String, mainClass: String, mainComponents: Array[String], crossVersionedValue: xsbti.CrossValue, classpathExtra: Array[File]) extends xsbti.ApplicationID {
+
+final case class AppID(
+  groupID: String,
+  name: String,
+  version: String,
+  mainClass: String,
+  mainComponents: Array[String],
+  crossVersionedValue: xsbti.CrossValue,
+  classpathExtra: Array[File]
+) extends xsbti.ApplicationID {
   def crossVersioned: Boolean = crossVersionedValue != xsbti.CrossValue.Disabled
 }
 
 object Application {
-  def apply(id: xsbti.ApplicationID): Application =
-    Application(id.groupID, id.name, new Explicit(id.version), id.mainClass, id.mainComponents.toList,
-      id.crossVersionedValue, id.classpathExtra)
+  def apply(id: xsbti.ApplicationID): Application = {
+    import id._
+    val version = new Explicit(id.version)
+    val mainComponents = id.mainComponents.toList
+    Application(groupID, name, version, mainClass, mainComponents, crossVersionedValue, classpathExtra)
+  }
 }
 
 object Repository {
-  trait Repository extends xsbti.Repository {
-    def bootOnly: Boolean
-  }
-  final case class Maven(id: String, url: URL, bootOnly: Boolean = false) extends xsbti.MavenRepository with Repository
-  final case class Ivy(id: String, url: URL, ivyPattern: String, artifactPattern: String, mavenCompatible: Boolean, bootOnly: Boolean = false, descriptorOptional: Boolean = false, skipConsistencyCheck: Boolean = false) extends xsbti.IvyRepository with Repository
-  final case class Predefined(id: xsbti.Predefined, bootOnly: Boolean = false) extends xsbti.PredefinedRepository with Repository
+  trait Repository extends xsbti.Repository { def bootOnly: Boolean }
+
+  final case class Maven(id: String, url: URL, bootOnly: Boolean = false)
+    extends xsbti.MavenRepository
+    with Repository
+
+  final case class Ivy(
+    id: String,
+    url: URL,
+    ivyPattern: String,
+    artifactPattern: String,
+    mavenCompatible: Boolean,
+    bootOnly: Boolean = false,
+    descriptorOptional: Boolean = false,
+    skipConsistencyCheck: Boolean = false
+  ) extends xsbti.IvyRepository
+    with Repository
+
+  final case class Predefined(id: xsbti.Predefined, bootOnly: Boolean = false)
+    extends xsbti.PredefinedRepository
+    with Repository
+
   object Predefined {
     def apply(s: String): Predefined = new Predefined(xsbti.Predefined.toValue(s), false)
     def apply(s: String, bootOnly: Boolean): Predefined = new Predefined(xsbti.Predefined.toValue(s), bootOnly)
   }
 
-  def isMavenLocal(repo: xsbti.Repository) = repo match { case p: xsbti.PredefinedRepository => p.id == xsbti.Predefined.MavenLocal; case _ => false }
-  def defaults: List[xsbti.Repository] = xsbti.Predefined.values.map(x => Predefined(x, false)).toList
+  def isMavenLocal(repo: xsbti.Repository): Boolean = repo match {
+    case p: xsbti.PredefinedRepository => p.id == xsbti.Predefined.MavenLocal
+    case _                             => false
+  }
+
+  def defaults: List[xsbti.Repository] = xsbti.Predefined.values.map(Predefined(_)).toList
 }
 
 final case class Search(tpe: Search.Value, paths: List[File])
+
 object Search extends Enumeration {
-  def none = Search(Current, Nil)
   val Only = value("only")
   val RootFirst = value("root-first")
   val Nearest = value("nearest")
   val Current = value("none")
+
   def apply(s: String, paths: List[File]): Search = Search(toValue(s), paths)
+  def none: Search = Search(Current, Nil)
 }
 
-final case class BootSetup(directory: File, lock: Boolean, properties: File, search: Search, promptCreate: String, enableQuick: Boolean, promptFill: Boolean) {
-  def map(f: File => File) = BootSetup(f(directory), lock, f(properties), search, promptCreate, enableQuick, promptFill)
+final case class BootSetup(
+  directory: File,
+  lock: Boolean,
+  properties: File,
+  search: Search,
+  promptCreate: String,
+  enableQuick: Boolean,
+  promptFill: Boolean
+) {
+  def map(f: File => File): BootSetup =
+    BootSetup(f(directory), lock, f(properties), search, promptCreate, enableQuick, promptFill)
 }
-final case class AppProperty(name: String)(val quick: Option[PropertyInit], val create: Option[PropertyInit], val fill: Option[PropertyInit])
+
+final case class AppProperty(name: String)(
+  val quick: Option[PropertyInit],
+  val create: Option[PropertyInit],
+  val fill: Option[PropertyInit]
+)
 
 sealed trait PropertyInit
 final class SetProperty(val value: String) extends PropertyInit
 final class PromptProperty(val label: String, val default: Option[String]) extends PropertyInit
 
 final class Logging(level: LogLevel.Value) extends Serializable {
-  def log(s: => String, at: LogLevel.Value) = if (level.id <= at.id) stream(at).println("[" + at + "] " + s)
-  def debug(s: => String) = log(s, LogLevel.Debug)
+  def log(s: => String, at: LogLevel.Value): Unit =
+    if (level.id <= at.id) stream(at).println("[" + at + "] " + s)
+
+  def debug(s: => String): Unit = log(s, LogLevel.Debug)
+
   private def stream(at: LogLevel.Value) = if (at == LogLevel.Error) System.err else System.out
 }
+
 object LogLevel extends Enumeration {
   val Debug = value("debug", 0)
   val Info = value("info", 1)
   val Warn = value("warn", 2)
   val Error = value("error", 3)
+
   def apply(s: String): Logging = new Logging(toValue(s))
 }
 
-final class AppConfiguration(val arguments: Array[String], val baseDirectory: File, val provider: xsbti.AppProvider) extends xsbti.AppConfiguration
+final class AppConfiguration(
+  val arguments: Array[String],
+  val baseDirectory: File,
+  val provider: xsbti.AppProvider
+) extends xsbti.AppConfiguration
